a435a76cabd59321421041166f44c6fbb949122f
Songtuan Lin <songtuan.lin@anu.edu.au>
2022-06-15 16:18:09 +1000

    update gitinnore

--- read_inputs.py
+++ read_inputs.py
@@ -29,7 +29,6 @@ if not os.path.exists(domainDir):
 visitedNumFile = 0
 for filename in tqdm(filenames):
     visitedNumFile = visitedNumFile + 1
-    # absFilePath = os.path.join(planFilePath, filename)
     with open(filename) as f:
         domainFile = f.readline().strip()
         problemFile = f.readline().strip()
@@ -41,9 +40,7 @@ for filename in tqdm(filenames):
         temp = domainFile
         domainFile = problemFile
         problemFile = temp
-    # print("problem file name>>>>>>>>>>>>>>>> " + problemFile + "\n")
     domainName = problemFile.split("/")[-2]
-    # print("domain file name>>>>>>>>>>>>>>>>> " + domainFile + "\n")
     parentDir = os.path.join(domainDir, domainName)
     if not os.path.exists(parentDir):
         os.mkdir(parentDir)
@@ -52,22 +49,17 @@ for filename in tqdm(filenames):
     newProblemDir = os.path.join(pfileDir, "problem")
     parsedDir = os.path.join(pfileDir, "parsed")
     groundedDir = os.path.join(pfileDir, "grounded")
-    # planDir = os.path.join(parentDir, pfileDir, "plans")
     if not os.path.exists(pfileDir):
         os.mkdir(pfileDir)
         os.mkdir(parsedDir)
         os.mkdir(groundedDir)
-        # if not os.path.exists(planDir):
-        #     os.mkdir(planDir)
     absParseOutputPath = os.path.join(parsedDir, pfileName + "." + "htn")
-    # absGroundOutputPath = os.path.join(groundedDir, pfileName + "." + "sas")
     absDomainPath = os.path.join(args.domainRoot, domainFile)
     absProblemPath = os.path.join(args.domainRoot, problemFile)
 
     numExistedPlans = len(os.listdir(groundedDir))
     planFileIndex = "plan_{n}".format(n = numExistedPlans + 1)
     groundedDirPerPlan = os.path.join(groundedDir, planFileIndex)
-    # planFileName = "plan_{n}.txt".format(n = numExistedPlans + 1)
     os.mkdir(groundedDirPerPlan)
     absPlanFilePath = os.path.join(groundedDirPerPlan, "plan.txt")
     with open(absPlanFilePath, "w") as f:
@@ -82,7 +74,6 @@ for filename in tqdm(filenames):
                 action = action[:-1]
             f.write("(" + action + ")" + "\n")
     planFileForGrounder = os.path.abspath("plan")
-    # pdb.set_trace()
 
     if not os.path.exists(absParseOutputPath):
         parserPath = args.parser
@@ -97,34 +88,6 @@ for filename in tqdm(filenames):
     
     grounderPath = args.grounder
     execGrounder = "./{}".format(os.path.relpath(grounderPath))
-    # subprocess.run([execGrounder, "--plan", absPlanFilePath, "-t", "-D", absParseOutputPath, absGroundOutputPath])
     subprocess.run([execGrounder, "-t", "-D", absParseOutputPath, absGroundOutputPath, "-P", planFileForGrounder], capture_output=True)
-
-    # if not os.path.exists(absGroundOutputPath):
-    #     grounderPath = os.path.join(prefix, "pandaPIgrounder/pandaPIgrounder")
-    #     parserPath = os.path.join(prefix, "pandaPIparser/pandaPIparser")
-
-    #     if not os.path.exists(newDomainDir):
-    #         os.mkdir(newDomainDir)
-    #     if not os.path.exists(newProblemDir):
-    #         os.mkdir(newProblemDir)
-    #     subprocess.run(["cp", absDomainPath, newDomainDir])
-    #     subprocess.run(["cp", absProblemPath, newProblemDir])
-
-    #     execGrounder = "./{}".format(os.path.relpath(grounderPath))
-    #     execParser = "./{}".format(os.path.relpath(parserPath))
-    #     subprocess.run([execParser, absDomainPath, absProblemPath, absParseOutputPath], capture_output=True)
-
-    #     subprocess.run([execGrounder, "-t", "-D", absParseOutputPath, absGroundOutputPath])
-    #     # subprocess.run([execGrounder, "-E", "-D", absParseOutputPath, absGroundOutputPath], capture_output=True)
-
-    # numExistedPlans = len(os.listdir(planDir))
-    # planFileName = "plan_{n}.txt".format(n = numExistedPlans + 1)
-    # absPlanFilePath = os.path.join(planDir, planFileName)
-    # with open(absPlanFilePath, "w") as f:
-    #     f.write(plan)
     if visitedNumFile >= numFile:
         break
-
-# print("Number of instances read: ")
-# print(visitedNumFile)
