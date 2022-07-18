eafaf3fc847568c29c065cb934656187b292cbef
Songtuan Lin <songtuan,lin@gmail.com>
2022-07-18 22:13:57 +1000

    multprocesses eval for the whole pipe

---
+++ eval.py
@@ -0,0 +1,172 @@
+import os
+import argparse
+import subprocess
+import multiprocessing
+
+from tqdm import tqdm
+from functools import partial
+
+parser = argparse.ArgumentParser(description='Arguments for Plan Verification Evaluators')
+parser.add_argument("benchmarks", type=str, help="Specify the input benchmark directory")
+parser.add_argument("-n", "--num_workers", default=None, type=int, help="Number of threads to run the evaluator")
+
+args = parser.parse_args()
+
+def collect_instances():
+    instance_dirs = []
+    for domain in os.listdir(args.benchmarks):
+        domain_dir = os.path.join(args.benchmarks, domain)
+        for problem in os.listdir(domain_dir):
+            problem_dir = os.path.join(domain_dir, problem)
+            for instance in os.listdir(problem_dir):
+                instance_dir = os.path.join(problem_dir, instance)
+                instance_dirs.append(instance_dir)
+    return instance_dirs
+
+def run_pl(lock, instance_dir):
+    is_succeed_run = True
+    domain_name = instance_dir.split("/")[-3]
+    problem_name = instance_dir.split("/")[-2]
+    plan_ind = instance_dir.split("/")[-1].split("_")[-1]
+    # evaluation info directory
+    output_dir = os.path.join(instance_dir, "eval-info")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    # output info file
+    eval_info_file = os.path.join(output_dir, "eval-info-pl.txt")
+    cmd_file = os.path.join(instance_dir, "planning.sh")
+    # run the plan verifier
+    cmd = "time timeout 600 bash " + cmd_file
+    proc = subprocess.Popen(cmd, executable="/bin/bash", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    outs, errs = proc.communicate()
+    if proc.returncode != 0:
+        # some error happens
+        err_info_file = os.path.join(output_dir, "err-info-pl.txt")
+        is_succeed_run = False
+        # print("non-zero returncore" + instance_dir)
+        with open(err_info_file, "w") as ef:
+            ef.write(outs)
+            ef.write(errs)
+        lock.acquire()
+        try:
+            with open("err-log-pl.txt", "a") as el:
+                el.write("Domain: {}\tProblem: {}\tPlan: {}\tIncorrect Ins: {}\n".format(domain_name, problem_name, plan_ind, False))
+        finally:
+            lock.release()
+    else:
+        is_solved = False
+        for line in outs.split("\n"):
+            if "- Status: Solved" in line:
+                is_solved = True
+        if not is_solved:
+            is_succeed_run = False
+            err_info_file = os.path.join(output_dir, "err-log-pl.txt")
+            with open(err_info_file, "w") as ef:
+                ef.write(outs)
+                ef.write(errs)
+            lock.acquire()
+            try:
+                with open("err-log-pl.txt", "a") as el:
+                    el.write("Domain: {}\tProblem: {}\tPlan: {}\tIncorrect Ins: {}\n".format(domain_name, problem_name, plan_ind, True))
+            finally:
+                lock.release()
+        else:
+            times = [e for e in errs.split("\n") if e]
+            wallTime = times[-3].split('\t')[-1]
+            minutes, seconds = wallTime.split("m")[0], wallTime.split("m")[-1][:-1]
+            total_seconds = float(minutes) * 60 + float(seconds)
+            with open(eval_info_file, "w") as of:
+                of.write(outs)
+            lock.acquire()
+            try:
+                with open("time-log-pl.txt", "a") as tl:
+                    tl.write("Domain: {}\tProblem: {}\tPlan: {}\tTime: {}\n".format(domain_name, problem_name, plan_ind, total_seconds))
+            finally:
+                lock.release()
+    return is_succeed_run
+
+def run_cyk(lock, instance_dir):
+    is_succeed_run = True
+    domain_name = instance_dir.split("/")[-3]
+    problem_name = instance_dir.split("/")[-2]
+    plan_ind = instance_dir.split("/")[-1].split("_")[-1]
+    # evaluation info directory
+    output_dir = os.path.join(instance_dir, "eval-info")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    # output info file
+    eval_info_file = os.path.join(output_dir, "eval-info-cyk.txt")
+    cmd_file = os.path.join(instance_dir, "cyk.sh")
+    # run the plan verifier
+    cmd = "time timeout 600 bash " + cmd_file
+    proc = subprocess.Popen(cmd, executable="/bin/bash", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    outs, errs = proc.communicate()
+    if proc.returncode != 0:
+        # some error happens
+        err_info_file = os.path.join(output_dir, "err-info-cyk.txt")
+        is_succeed_run = False
+        # print("non-zero returncore" + instance_dir)
+        with open(err_info_file, "w") as ef:
+            ef.write(outs)
+            ef.write(errs)
+        lock.acquire()
+        try:
+            with open("err-log-cyk.txt", "a") as el:
+                el.write("Domain: {}\tProblem: {}\tPlan: {}\tIncorrect Ins: {}\n".format(domain_name, problem_name, plan_ind, False))
+        finally:
+            lock.release()
+    else:
+        lines = [line for line in outs.split("\n") if line]
+        if lines[-1] != "The given plan is a solution":
+            is_succeed_run = False
+            err_info_file = os.path.join(output_dir, "err-log-cyk.txt")
+            with open(err_info_file, "w") as ef:
+                ef.write(outs)
+                ef.write(errs)
+            lock.acquire()
+            try:
+                with open("err-log-cyk.txt", "a") as el:
+                    el.write("Domain: {}\tProblem: {}\tPlan: {}\tIncorrect Ins: {}\n".format(domain_name, problem_name, plan_ind, True))
+            finally:
+                lock.release()
+        else:
+            times = [e for e in errs.split("\n") if e]
+            wallTime = times[-3].split('\t')[-1]
+            minutes, seconds = wallTime.split("m")[0], wallTime.split("m")[-1][:-1]
+            total_seconds = float(minutes) * 60 + float(seconds)
+            with open(eval_info_file, "w") as of:
+                of.write(outs)
+            lock.acquire()
+            try:
+                with open("time-log-cyk.txt", "a") as tl:
+                    tl.write("Domain: {}\tProblem: {}\tPlan: {}\tTime: {}\n".format(domain_name, problem_name, plan_ind, total_seconds))
+            finally:
+                lock.release()
+    return is_succeed_run
+
+def start_pl(instance_dirs):
+    num_cpus = multiprocessing.cpu_count()
+    lock = multiprocessing.Manager().Lock()
+    # instance_dirs = [(lock, instance_dir) for instance_dir in instance_dirs]
+    if args.num_workers is not None:
+        num_workers = args.num_workers
+    else:
+        num_workers = num_cpus
+    with multiprocessing.Pool(num_workers) as p:
+        r = list(tqdm(p.imap(partial(run_pl, lock), instance_dirs), total=len(instance_dirs)))
+
+def start_cyk(instance_dirs):
+    num_cpus = multiprocessing.cpu_count()
+    lock = multiprocessing.Manager().Lock()
+    # instance_dirs = [(lock, instance_dir) for instance_dir in instance_dirs]
+    if args.num_workers is not None:
+        num_workers = args.num_workers
+    else:
+        num_workers = num_cpus
+    with multiprocessing.Pool(num_workers) as p:
+        r = list(tqdm(p.imap(partial(run_cyk, lock), instance_dirs), total=len(instance_dirs)))
+
+if __name__ == "__main__":
+    instance_dirs = collect_instances()
+    start_cyk(instance_dirs)
+    start_pl(instance_dirs)
---
+++ preprocess.py
@@ -0,0 +1,100 @@
+import os
+import argparse
+import subprocess
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='Arguments for the Evaluation')
+parser.add_argument("--benchmarks", nargs="+", type=str, help="Directories of benchmarks")
+parser.add_argument("--root", type=str, help="Root directories of domains")
+parser.add_argument("--parser", type=str, help="Path to the parser")
+parser.add_argument("--grounder", type=str, help="Path to the grounder")
+parser.add_argument("--transformer", type=str, help="Path to the transformer")
+parser.add_argument("--verifier", type=str, help="Path to the verifier")
+parser.add_argument("--planner", type=str, help="Path to the planner")
+parser.add_argument("--output", type=str, help="Output directory")
+args = parser.parse_args()
+
+
+def parse_instance(ins, od, rd, dirs):
+    with open(ins, "r") as f:
+        df = f.readline().strip() # domain file
+        pf = f.readline().strip() # problem file
+        pl = f.readline() # plan
+    dfn = df.split("/")[-1].split(".")[0] # domain file name
+    pfn = pf.split("/")[-1].split(".")[0] # problem file name
+    if "domain" in pfn:
+        # domain file and problem file are in incorrect order
+        pfn = dfn
+        df, pf = pf, df
+    df = os.path.join(rd, df)
+    pf = os.path.join(rd, pf)
+    dn = pf.split("/")[-2] # domain name
+    ddir = os.path.join(od, dn) # output domain directory 
+    if not os.path.exists(ddir):
+        os.mkdir(ddir)
+    pdir = os.path.join(ddir, pfn) # problem directory within the domain directory
+    dfdir = os.path.join(pdir, "domain")
+    pfdir = os.path.join(pdir, "problem")
+    grodir = os.path.join(pdir, "grounded")
+    pardir = os.path.join(pdir, "parser")
+    if not os.path.exists(pdir):
+        os.mkdir(pdir)
+    np = len(os.listdir(pdir)) # number of parsed plan instances
+    insdir = os.path.join(pdir, "plan_{}".format(np + 1))
+    os.mkdir(insdir)
+    cykpf = os.path.join(insdir, "plan-cyk.txt") # plan format for cyk verifier
+    plpf = os.path.join(insdir, "plan-planning.txt") # plan format for planning-based verifier
+    with open(cykpf, "w") as f:
+        f.write(pl)
+    actions = pl.split(";")
+    with open(plpf, "w") as f:
+        for action in actions:
+            if action[-1] == "\n":
+                action = action[:-1]
+            f.write("(" + action + ")" + "\n")
+
+    parf = os.path.join(insdir, "{}.htn".format(pfn)) # parsed problem file
+    # command for parsing
+    cmdp_cyk = args.parser + " " + df + " " + pf + " " + parf
+    cmdp_pl = cmdp_cyk 
+
+    grof_cyk = os.path.join(insdir, "{}-cyk.sas".format(pfn)) # grounded problem file for cyk
+    grof_pl = os.path.join(insdir, "{}-planning.sas".format(pfn)) # founded problem file for planning-based
+    # command for grounding
+    cmdg_cyk = args.grounder + " " + "-t -D" + " " + parf + " " + grof_cyk + " " + "-P" + " " + plpf
+    cmdg_pl = args.grounder + " " + "-D" + " " + parf + " " + grof_pl + " " + "-P" + " " + plpf
+    # command calling cyk-verifier
+    cmdv_cyk = args.verifier + " " + "-h" + " " + grof_cyk + " " + "-p" + " " + cykpf + " " + "-v to-verifier"
+    # command calling transformer 
+    cmd_trans = args.transformer + " " + grof_pl + " " + plpf
+    # transformed file
+    trf = os.path.join("{}.verify".format(plpf))
+    cmdv_pl = args.planner + " " + trf + " --suboptimal --gValue=none"
+
+    cyk_sh = os.path.join(insdir, "cyk.sh")
+    pl_sh = os.path.join(insdir, "planning.sh")
+    with open(cyk_sh, "w") as f:
+        f.write("ulimit -v 8388608\n")
+        f.write(cmdp_cyk + "\n")
+        f.write(cmdg_cyk + "\n")
+        f.write(cmdv_cyk)
+    with open(pl_sh, "w") as f:
+        f.write("ulimit -v 8388608\n")
+        f.write(cmdp_pl + "\n")
+        f.write(cmdg_pl + "\n")
+        f.write(cmd_trans + "\n")
+        f.write(cmdv_pl)
+    dirs.append(insdir)
+
+def collect_instances(dirs):
+    inss = []
+    for d in dirs:
+        for p in os.listdir(d):
+            inss.append(os.path.join(d, p))
+    return inss
+    
+if __name__ == "__main__":
+    inss = collect_instances(args.benchmarks)
+    dirs = []
+    for ins in tqdm(inss):
+        parse_instance(ins, args.output, args.root, dirs)
